6fc636aa1a11046a1c5471f2393ea4f7a8b3c34a
basiaszafraniec <basia.szafraniec@gmail.com>
2024-09-23 20:59:00 +0200

    updated package.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2c33b3f452accb54"; }
+/******/ 		__webpack_require__.h = function() { return "01b61fb61c57ee32"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
