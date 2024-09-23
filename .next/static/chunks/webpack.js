4e8f9493a41325385bcb695c7de33035d6f7479c
basiaszafraniec <basia.szafraniec@gmail.com>
2024-09-23 20:38:51 +0200

    deleted next.config

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2c33b3f452accb54"; }
+/******/ 		__webpack_require__.h = function() { return "6b93b037caf331d0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f0e0f3af71f9566a.hot-update.js
+++ .next/static/webpack/webpack.f0e0f3af71f9566a.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "2c33b3f452accb54"; }
+/******/ 	__webpack_require__.h = function() { return "6b93b037caf331d0"; }
 /******/ }();
 /******/ 
 /******/ }
