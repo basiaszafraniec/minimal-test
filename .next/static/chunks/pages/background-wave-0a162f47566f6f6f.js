ccdceb01992080a75ae6f11d038c774d1fd1e0e1
basiaszafraniec <basia.szafraniec@gmail.com>
2024-09-30 12:37:31 +0200

    built

---
+++ .next/static/chunks/pages/background-wave-0a162f47566f6f6f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[334],{2327:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/background-wave",function(){return u(1054)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=2327)}),_N_E=n.O()}]);
